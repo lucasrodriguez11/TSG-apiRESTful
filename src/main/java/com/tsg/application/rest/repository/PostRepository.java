7f2ee222db855bea178568a44abf4e6ae4e02351
Lucas Nahuel <rodriguezlucas011@gmail.com>
2025-02-23 16:32:41 -0300

    feat(repositories): creación de repositorios para usuarios y post

---
+++ src/main/java/com/tsg/application/rest/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.tsg.application.rest.repository;
+
+
+import com.tsg.application.rest.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserId(Long userId);
+}
